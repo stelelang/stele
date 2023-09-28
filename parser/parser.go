ba77667f8b4568e6f9571202e65c0d8c486b7a78
DeedleFake <deedlefake@users.noreply.github.com>
2023-09-28 19:37:40 -0400

    parser: implement parsing of imports

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
+	"fmt"
 	"io"
+	"path/filepath"
 
 	"deedles.dev/stele"
 	"deedles.dev/stele/scanner"
@@ -14,11 +16,70 @@ func Parse(r io.Reader) (script stele.Script, err error) {
 }
 
 type parser struct {
-	s *scanner.Scanner
+	s   *scanner.Scanner
+	buf scanner.Token
+}
+
+func (p *parser) next() (scanner.Token, bool) {
+	if p.buf.Type != scanner.INVALID {
+		tok := p.buf
+		p.buf = scanner.Token{}
+		return tok, true
+	}
+
+	ok := p.s.Scan()
+	if err := p.s.Err(); err != nil {
+		p.throw(fmt.Errorf("scan for next token: %w", err))
+	}
+	return p.s.Tok(), ok
+}
+
+func (p *parser) expect(t scanner.Type) scanner.Token {
+	tok, ok := p.next()
+	if !ok {
+		p.throw(fmt.Errorf("expected %v but found end of input", t))
+	}
+	if (t >= 0) && (tok.Type != t) {
+		p.throw(fmt.Errorf("expected %v but found %v", t, tok.Type))
+	}
+
+	return tok
 }
 
 func (p *parser) parseScript() stele.Script {
-	panic("not implemented")
+	var script stele.Script
+	for {
+		tok, ok := p.next()
+		if !ok {
+			return script
+		}
+
+		switch tok.Type {
+		case scanner.IMPORT:
+			script.Declarations.Add(p.parseImport())
+		default:
+			p.throw(UnexpectedTokenError{tok})
+		}
+	}
+}
+
+func (p *parser) parseImport() (id, path string) {
+	path = p.expect(scanner.STRING).Val.(string)
+
+	tok := p.expect(-1)
+	switch tok.Type {
+	case scanner.AS:
+		id = p.expect(scanner.IDENT).Val.(string)
+		p.expect(scanner.SEMI)
+		return id, path
+
+	case scanner.SEMI:
+		return filepath.Base(path), path
+
+	default:
+		p.throw(UnexpectedTokenError{tok})
+		return
+	}
 }
 
 func (p *parser) throw(err error) {
@@ -37,3 +98,11 @@ func (p *parser) catch(err *error) {
 }
 
 type parseErr struct{ err error }
+
+type UnexpectedTokenError struct {
+	tok scanner.Token
+}
+
+func (err UnexpectedTokenError) Error() string {
+	return fmt.Sprintf("unexpected token: %v", err.tok.Val)
+}
--- script.go
+++ script.go
@@ -1,3 +1,29 @@
 package stele
 
-type Script struct{}
+import "deedles.dev/mk"
+
+type Script struct {
+	Declarations Declarations
+}
+
+type Declarations struct {
+	m map[string]any
+}
+
+func (d *Declarations) init() {
+	if d.m == nil {
+		mk.Map(&d.m, 0)
+	}
+}
+
+func (d *Declarations) Add(id string, val any) bool {
+	d.init()
+
+	_, ok := d.m[id]
+	if ok {
+		return false
+	}
+
+	d.m[id] = val
+	return true
+}
