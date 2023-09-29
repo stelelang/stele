fef20ddca96f87a1b6f81a19401dda2073b5bb65
DeedleFake <deedlefake@users.noreply.github.com>
2023-09-29 12:55:28 -0400

    stele: add some godoc comments

--- stmt.go
+++ stmt.go
@@ -1,11 +1,22 @@
 package stele
 
+// A Stmt is an executable piece of code.
 type Stmt interface {
+	// Eval evaluates the Stmt in the context of the given State and
+	// possibly returns a value. If the Stmt does not return values, the
+	// returned Value will be the zero-value Value.
 	Eval(*State) Value
 }
 
+// An Expr is an executable piece of code that returns a value. All
+// Exprs are Stmts, but not all Stmts are Exprs.
 type Expr interface {
+	// Type is the type of the value returned by the Expr.
 	Type() Type
+
+	// Eval evaluates the Expr in the context of the given State and
+	// returns a value. Unlike a Stmt, an Expr _must_ return a valid
+	// Value.
 	Eval(*State) Value
 }
 
